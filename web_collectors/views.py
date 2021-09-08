c125603029abfbc3a8ec3801100b989e98e8dc16
Инара <inarashugaikina@gmail.com>
2021-09-08 21:36:17 +0600

    Creating functionality for main page

--- web_collectors/views.py
+++ web_collectors/views.py
@@ -1,6 +1,14 @@
+from django.conf import settings
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from web_collectors.models import Collection
+
 
 def index(request):
-    return HttpResponse('Главная страница')
+    collections = Collection.objects.order_by('-creation_date')[:10]
+    paginator = Paginator(collections, settings.ITEMS_PER_PAGE)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    return render(request, 'index.html', {'page': page, 'paginator': paginator})
