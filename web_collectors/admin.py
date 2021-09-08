c98786b7fbb7bd3dc368b636d2faf9d2dd2dc5da
Инара <inarashugaikina@gmail.com>
2021-09-08 21:14:24 +0600

    Registering models in admin zone

--- web_collectors/admin.py
+++ web_collectors/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Collection, CollectionItem, Photo
+
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'creation_date', 'collection_owner', 'photo')
+    list_filter = ('name', 'collection_owner', )
+    search_fields = ('name', )
+
+
+class PhotoAdminInLine(admin.TabularInline):
+    model = Photo
+    extra = 1
+
+
+class CollectionItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'collection', 'creation_date')
+    list_filter = ('name', 'collection', )
+    search_fields = ('name', 'collection', )
+    inlines = (PhotoAdminInLine, )
+
+
+admin.site.register(Collection, CollectionAdmin)
+admin.site.register(CollectionItem, CollectionItemAdmin)
