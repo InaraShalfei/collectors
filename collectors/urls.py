e8bffe967dfa41893189ea3b66451af957a14dbb
Инара <inarashugaikina@gmail.com>
2021-09-08 20:55:58 +0600

    Fixing urls.py

--- collectors/urls.py
+++ collectors/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('web_collectors.urls'), namespace='web_collectors'),
+    path('', include('web_collectors.urls')),
 ]
