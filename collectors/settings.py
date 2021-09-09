2f42feb616bd6684287727bac69f963976d337cf
Инара <inarashugaikina@gmail.com>
2021-09-09 16:19:26 +0600

    Adding information about login url in settings.py

--- collectors/settings.py
+++ collectors/settings.py
@@ -119,6 +119,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_URL = '/auth/login/'
+
+LOGIN_REDIRECT_URL = 'index'
+
+LOGOUT_REDIRECT_URL = 'index'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
