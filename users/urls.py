e7a48afac5acadc796fb11506a23262cebc4be97
Инара <inarashugaikina@gmail.com>
2021-09-09 16:18:57 +0600

    Changing users urls

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from users import views
@@ -6,9 +5,5 @@ from users import views
 app_name = 'users'
 
 urlpatterns = [
-    path(
-      'logout/', LogoutView.as_view(template_name='users/logged_out.html'),
-      name='logout'
-    ),
     path('signup/', views.SignUp.as_view(), name='signup'),
 ]
