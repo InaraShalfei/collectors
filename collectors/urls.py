bf2164d3d8f700a5cc17a6fa9e22bf353252237c
Инара <inarashugaikina@gmail.com>
2021-09-09 14:29:40 +0600

    Creating users app and changing html for logout and signup

--- collectors/urls.py
+++ collectors/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls', namespace='users')),
+    path('auth/', include('django.contrib.auth.urls')),
     path('', include('web_collectors.urls')),
 ]
--- web_collectors/urls.py
+++ web_collectors/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'web_collectors'
+#app_name = 'web_collectors'
 
 urlpatterns = [
     path('', views.index, name='index'),
