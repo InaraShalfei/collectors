117897578cfc5be53bc107f78d95b86b9e9c9561
Инара <inarashugaikina@gmail.com>
2021-09-10 12:16:54 +0600

    Adding information about static dir into settings.py

--- collectors/settings.py
+++ collectors/settings.py
@@ -133,8 +133,13 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ITEMS_PER_PAGE = 10
