1fb489ea497840bbfeb2f13d01f17af9965cd9cb
Инара <inarashugaikina@gmail.com>
2021-09-14 12:27:46 +0600

    Adding urls for collection page and media route

--- collectors/urls.py
+++ collectors/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +25,10 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('web_collectors.urls', namespace='web_collectors')),
     path('', include('static_pages.urls', namespace='static_pages')),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
--- web_collectors/urls.py
+++ web_collectors/urls.py
@@ -7,5 +7,7 @@ app_name = 'web_collectors'
 urlpatterns = [
     path('', views.index, name='index'),
     path('group/<slug:slug>/', views.collection_group, name='group'),
+    path('group/<slug:slug>/<str:collection_name>', views.collection, name='collection'),
     path('groups/', views.collection_groups, name='groups'),
+
 ]
