6c1f7a85cfed53556efcc14299c94cad768214c8
Инара <inarashugaikina@gmail.com>
2021-09-18 15:02:30 +0600

    Change representation of photo field in forms

--- web_collectors/forms.py
+++ web_collectors/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Collection, CollectionItem
+from .models import Collection, CollectionItem, Photo
 
 
 class CollectionForm(forms.ModelForm):
@@ -19,8 +19,9 @@ class CollectionForm(forms.ModelForm):
 class ItemForm(forms.ModelForm):
     class Meta:
         model = CollectionItem
-        fields = ('name', 'description', )
+        fields = ('name', 'description', 'photo', )
         description = forms.CharField(max_length=100, widget=forms.Textarea)
+        photo = forms.MultipleChoiceField()
 
     def clean_data(self):
         name = self.cleaned_data['name']
