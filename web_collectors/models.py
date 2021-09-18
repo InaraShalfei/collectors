443326b2b6071f4e02e224e2067e71bbd45a8222
Инара <inarashugaikina@gmail.com>
2021-09-18 15:02:43 +0600

    Change relations between two models

--- web_collectors/models.py
+++ web_collectors/models.py
@@ -32,24 +32,25 @@ class Collection(models.Model):
         return self.name
 
 
+class Photo(models.Model):
+    photo = models.ImageField(upload_to='media/photo/', blank=True, null=True)
+    #item = models.ForeignKey(CollectionItem, on_delete=models.CASCADE, related_name='photos')
+    position = models.AutoField(primary_key=True)
+
+    class Meta:
+        ordering = ['position']
+
+
 class CollectionItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=200, blank=True)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE, related_name='collection_items')
     creation_date = models.DateTimeField(auto_created=False, default=timezone.now)
     position = models.AutoField(primary_key=True)
+    photo = models.ManyToManyField(Photo)
 
     class Meta:
         ordering = ['position']
 
     def __str__(self):
         return self.name
-
-
-class Photo(models.Model):
-    photo = models.ImageField(upload_to='media/photo', blank=True, null=True, )
-    item = models.ForeignKey(CollectionItem, on_delete=models.CASCADE, related_name='photos')
-    position = models.AutoField(primary_key=True)
-
-    class Meta:
-        ordering = ['position']
