014796b0ec4a47b709d0d83a53ea8aedbb860766
Инара <inarashugaikina@gmail.com>
2021-10-21 13:43:04 +0600

    Rename similar urls for achieving them

--- web_collectors/urls.py
+++ web_collectors/urls.py
@@ -6,6 +6,7 @@ app_name = 'web_collectors'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('groups/', views.collection_groups, name='groups'),
     path('all/', views.all_authors, name='all_authors'),
     path('new/', views.create_collection, name='new_collection'),
     path('follow/', views.follow_index, name='follow_index'),
@@ -18,13 +19,13 @@ urlpatterns = [
     path('group/<slug:slug>/', views.collection_group, name='group'),
     path('group/<slug:slug>/<int:collection_id>', views.collection, name='collection'),
     path('group/<slug:slug>/<int:collection_id>/comment', views.add_comment, name='add_comment'),
-    path('group/<slug:slug>/<int:collection_id>/<int:comment_id>/delete', views.delete_comment, name='delete_comment'),
-    path('group/<slug:slug>/<int:collection_id>/<int:comment_id>/update', views.update_comment, name='update_comment'),
+    path('group/<slug:slug>/<int:collection_id>/<int:comment_id>/delete_comment', views.delete_comment, name='delete_comment'),
+    path('group/<slug:slug>/<int:collection_id>/<int:comment_id>/update_comment', views.update_comment, name='update_comment'),
     path('group/<slug:slug>/<int:collection_id>/edit', views.update_collection, name='update_collection'),
     path('group/<slug:slug>/<int:collection_id>/delete', views.delete_collection, name='delete_collection'),
     path('group/<slug:slug>/<int:collection_id>/new', views.create_item, name='new_item'),
     path('group/<slug:slug>/<int:collection_id>/<int:item_id>', views.collection_item, name='item'),
     path('group/<slug:slug>/<int:collection_id>/<int:item_id>/update', views.update_item, name='update_item'),
     path('group/<slug:slug>/<int:collection_id>/<int:item_id>/delete', views.delete_item, name='delete_item'),
-    path('groups/', views.collection_groups, name='groups'),
+
 ]
