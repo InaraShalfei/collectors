4e0e44ee43ed62841b06d7b38c8541999fc27918
Инара <inarashugaikina@gmail.com>
2021-11-08 23:41:56 +0600

    Creating tests for objects of Comment and Follow models

--- web_collectors/tests/test_models.py
+++ web_collectors/tests/test_models.py
@@ -1,6 +1,8 @@
+from django.db.utils import IntegrityError
+
 from django.test import TestCase
 
-from web_collectors.models import CollectionGroup, Collection, User, CollectionItem
+from web_collectors.models import CollectionGroup, Collection, User, CollectionItem, Comment, Follow
 
 
 class CollectionGroupTest(TestCase):
@@ -115,3 +117,73 @@ class CollectionItemTest(TestCase):
         expected_name = collection_item.name
         self.assertEqual(expected_name, str(collection_item))
 
+
+class CommentTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.group = CollectionGroup.objects.create(
+            name='Книги',
+            slug='',
+            description='All books in the world'
+        )
+        cls.user = User.objects.create_user(username='auth')
+        cls.collection = Collection.objects.create(
+            name='Russian authors',
+            description='All books of russian authors',
+            owner=cls.user,
+            group=cls.group
+        )
+        cls.comment = Comment.objects.create(
+            collection=cls.collection,
+            author=cls.user,
+            text='Cool!',
+        )
+
+    def test_verbose_names(self):
+        comment = CommentTest.comment
+        field_verboses= {
+            'collection': 'Название коллекции',
+            'author': 'Автор коллекции',
+            'text': 'Текст комментария',
+            'parent_comment': 'Ответный комментарий'
+        }
+        for field, expected_value in field_verboses.items():
+            with self.subTest(field=field):
+                self.assertEqual(comment._meta.get_field(field).verbose_name, expected_value)
+
+    def test_text_is_str(self):
+        comment = CommentTest.comment
+        expected_value = comment.text
+        self.assertEqual(expected_value, str(comment))
+
+
+class FollowTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user1 = User.objects.create_user(username='arm')
+        cls.user2 = User.objects.create_user(username='bcd')
+        cls.follow = Follow.objects.create(
+            user=cls.user1,
+            author=cls.user2
+        )
+
+    def test_verbose_names(self):
+        follow = FollowTest.follow
+        field_verboses = {
+            'user': 'Подписчик',
+            'author': 'Автор коллекции'
+        }
+        for field, expected_value in field_verboses.items():
+            with self.subTest(field=field):
+                self.assertEqual(follow._meta.get_field(field).verbose_name, expected_value)
+
+    def test_unique_constraint_relation(self):
+        user_1 = User.objects.create(username='123')
+        user_2 = User.objects.create(username='345')
+        Follow.objects.create(user=user_1, author=user_2)
+        follow = Follow(user=user_1, author=user_2)
+        with self.assertRaises(Exception) as raised:
+            follow.save()
+        self.assertEqual(IntegrityError, type(raised.exception))
