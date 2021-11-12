4b5e61a8848883c9267169fdac20b5edecb61b7a
Инара <inarashugaikina@gmail.com>
2021-11-12 21:07:45 +0600

    Testing paginator on homepage

--- web_collectors/tests/test_paginators.py
+++ web_collectors/tests/test_paginators.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.test import TestCase, Client
+from django.urls import reverse
 
 from web_collectors.models import CollectionGroup, User, Collection, CollectionItem
 
@@ -7,40 +10,47 @@ class PaginatorViewsTest(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.user = User.objects.create_user(username='Visitor')
         for i in range(12):
-            cls.group = CollectionGroup.objects.create(
-                name=f'Книги{1}',
-                slug=f'knigi{1}',
+            CollectionGroup.objects.create(
+                name=f'Книги{i}',
+                slug=f'knigi{i}',
                 description=f'All books in the world - {i}'
             )
         for i in range(12):
-            cls.user = User.objects.create_user(username=f'Boba - {i}')
+            User.objects.create_user(username=f'Boba - {i}')
+
+        group = CollectionGroup.objects.create(name='Стихи', slug='poems', description='Poems of all russian authors')
+        author = User.objects.create(username='Ira')
         for i in range(12):
-            cls.collection = Collection.objects.create(
+            collection = Collection(
                 name=f'Russian authors{i}',
                 description=f'All books of russian authors - {i}',
-                owner=User.objects.create(username='Ira'),
-                group=CollectionGroup.objects.create(
-                    name='Пластинки',
-                    slug='discs',
-                    description='Discs from all over the world'
-                ))
+                owner=author,
+                group=group)
+            collection.creation_date = datetime.datetime.now() - datetime.timedelta(minutes=15 - i)
+            collection.save()
+        collection = Collection.objects.create(name='Russian poems', description='All Russian poems',
+                                               owner=author,
+                                               group=group)
         for i in range(4):
-            cls.collection_item = CollectionItem.objects.create(
+            collection_item = CollectionItem(
                 name=f'Pushkin poems{i}',
                 description=f'Poems of A.S.Pushkin - {i}',
-                collection=Collection.objects.create(
-                    name='Russian poems',
-                    description='All Russian poems',
-                    owner=User.objects.create(username='Ira'),
-                    group=CollectionGroup.objects.create(
-                        name='Стихи',
-                        slug='poems',
-                        description='Poems of all russian authors'
-                    ),
-                    position={i}))
+                collection=collection,
+                position=i)
+            collection_item.creation_date = datetime.datetime.now() - datetime.timedelta(minutes=15 - i)
+            collection_item.save()
 
     def setUp(self):
         self.guest_client = Client()
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
+
+    def test_homepage_first_page_has_10_records(self):
+        response = self.guest_client.get(reverse('web_collectors:index'))
+        self.assertEqual(len(response.context['page']), 10)
+
+    def test_homepage_second_page_has_2_records(self):
+        response = self.guest_client.get(reverse('web_collectors:index') + '?page=2')
+        self.assertEqual(len(response.context['page']), 3)
