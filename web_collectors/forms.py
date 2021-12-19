9b6a827ca3d9db732398112fad03e3649a424fbf
Инара <inarashugaikina@gmail.com>
2021-12-19 20:50:25 +0600

    Make representation of non_field errors of form in templates

--- web_collectors/forms.py
+++ web_collectors/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import NON_FIELD_ERRORS
 from multiupload.fields import MultiImageField, MultiMediaField
 
 from .models import Collection, CollectionItem, Photo, Comment
@@ -10,24 +11,24 @@ class CollectionForm(forms.ModelForm):
         fields = ('name', 'description', 'photo', 'group')
         description = forms.CharField(max_length=100, widget=forms.Textarea)
 
-    def clean_data(self):
+    def clean_name(self):
         name = self.cleaned_data['name']
-        if '' in name:
-            raise forms.ValidationError('Необходимо заполнить данное поле формы!')
+        if name is None:
+            self.add_error(NON_FIELD_ERRORS, 'Необходимо заполнить данное поле формы!')
         return name
 
 
 class ItemForm(forms.ModelForm):
     class Meta:
         model = CollectionItem
-        fields = ('name', 'description' )
+        fields = ('name', 'description')
         description = forms.CharField(max_length=100, widget=forms.Textarea)
     photos = MultiMediaField(min_num=1, max_num=5, max_file_size=1024*1024*5, media_type='image')
 
-    def clean_data(self):
+    def clean_name(self):
         name = self.cleaned_data['name']
-        if '' in name:
-            raise forms.ValidationError('Необходимо заполнить данное поле формы!')
+        if name is None:
+            self.add_error(NON_FIELD_ERRORS, 'Необходимо заполнить данное поле формы!')
         return name
 
 
