9ed37e9ee87c059fb789fb053de47ce98fa24e28
InaraShalfei <inarashugaikina@gmail.com>
2022-11-17 20:21:04 +0600

    Limit queryset results represented by search providers

--- web_collectors/search_providers.py
+++ web_collectors/search_providers.py
@@ -7,20 +7,20 @@ from web_collectors.models import (CollectionGroup, Collection, CollectionItem,
 def group_search(el):
     return CollectionGroup.objects.filter(
             Q(name__icontains=el) | Q(description__icontains=el)
-        )
+        )[:3]
 
 
 def collection_search(el):
     return Collection.objects.filter(
             Q(name__icontains=el) | Q(description__icontains=el)
-        )
+        )[:3]
 
 
 def item_search(el):
     return CollectionItem.objects.filter(
             Q(name__icontains=el) | Q(description__icontains=el)
-        )
+        )[:3]
 
 
 def author_search(el):
-    return CustomUser.objects.filter(username__icontains=el)
+    return CustomUser.objects.filter(username__icontains=el)[:3]
