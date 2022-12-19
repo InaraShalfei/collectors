3cc1b18abf1fa09630c8e31fdda46b64f764517c
InaraShalfei <inarashugaikina@gmail.com>
2022-12-19 22:40:40 +0600

    Extract fake_image function to  a new file to be used in different tests

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -3,30 +3,15 @@ import tempfile
 
 
 from django.conf import settings
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import override_settings, TestCase, Client
 from django.urls import reverse
 
+from tests.fake_image import get_fake_image
 from web_collectors.forms import CollectionForm, CommentForm, ItemForm
 from web_collectors.models import (Collection, CollectionGroup, CustomUser,
                                    Comment, CollectionItem)
 
 TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
-
-
-def get_fake_image():
-    return SimpleUploadedFile(
-        name='small.gif',
-        content=(
-            b'\x47\x49\x46\x38\x39\x61\x02\x00'
-            b'\x01\x00\x80\x00\x00\x00\x00\x00'
-            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-            b'\x0A\x00\x3B'
-        ),
-        content_type='image/gif'
-    )
 
 
 @override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
